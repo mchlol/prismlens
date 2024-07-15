7a42678b8c3d2dd2b7a6e176f15578f4163649c4
Michelle Brown <m.andreabr@gmail.com>
2024-07-15 18:04:54 +1000

    add div bg images

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -7,23 +7,23 @@ export default function Hero() {
 
             <div className="hero--img-wrap flex gap-8 justify-center">
 
-                <div className="hero--img__left h-[350px] w-[302px] bg-gray-400 rounded-tl-lg rounded-tr-lg rounded-bl-lg rounded-br-[150px]"></div>
+                <div className="hero--img__left h-[350px] w-[302px] bg-[url('./assets/prism01.jpg')] bg-cover rounded-tl-lg rounded-tr-lg rounded-bl-lg rounded-br-[150px]"></div>
 
-                <div className="hero--img__right h-[350px] w-[566px] bg-gray-200 rounded-tl-[175px] rounded-br-[175px]"></div>
+                <div className="hero--img__right h-[350px] w-[566px] bg-[url('./assets/tarot01.jpg')] bg-cover rounded-tl-[175px] rounded-br-[175px]"></div>
 
             </div>
 
-            <div className="hero--logo-wrap border-2 border-red-500 z-10 absolute top-1/4 left-1/4">
+            <div className="hero--logo-wrap z-10 absolute top-1/4 left-1/4">
 
-                <img src={LogoFull} alt="logo" className="w-[500px] max-w-full" />
+                <img src={LogoFull} alt="logo" className="w-[500px] max-w-full svg-shadow" />
 
             </div>
 
         </div>
 
-        <div className="hero--text max-w-[70ch] mx-auto text-center mt-4">
+        <div className="hero--text max-w-[70ch] mx-auto text-center m-4 p-8">
 
-            <h2 className="text-xl font-averiaSerifLibre mb-4">Welcome to PrismLens, the ultimate tarot app for the modern queen! 🌟 </h2>
+            <h2 className="text-xl font-averiaSerifLibre mb-4">Welcome to PrismLens, the ultimate tarot app for the modern queen!</h2>
 
             <p>Dive into intuitive readings that promise to unlock your potential and guide you to your most fabulous self. Don't miss out on the insights that will keep you ahead of the game—let PrismLens light your path to empowerment and success.</p>
 
