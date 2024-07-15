be6df8a02fce0e0a4c005f8ec1166d650d5672de
Michelle Brown <m.andreabr@gmail.com>
2024-07-15 12:27:29 +1000

    add content and ext link to footer

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,9 +1,16 @@
+import MBLogo from '../assets/mblogo.svg';
+
+
 export default function Footer() {
     return(
-        <p>
+        <p className="flex justify-between p-4 border-t-2">
             <small>
                 <strong>Disclaimer:</strong> This app is for fun only and no advice given should be taken seriously. 
             </small>
+            
+            <a href="https://michelle-brown.dev/" target="_blank" rel="noopener noreferrer">
+                <img src={MBLogo} alt="MB logo" />
+            </a>
         </p>
     )
 }
