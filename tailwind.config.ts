8f1b56fa9684964efb542076257344cbf6335961
Michelle Brown <m.andreabr@gmail.com>
2024-07-15 18:05:07 +1000

    customise colours and bg images

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,11 +3,18 @@ import type { Config } from "tailwindcss";
 export default {
   content: ["./app/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    colors: {
+      blueblack: "#1A191F",
+      riderred: '#D31507',
+      ridergreen: '#B2D28F',
+      rideryellow: '#F5D60A',
+      ridercream: '#FAF9E5'
+    },
     extend: {
       fontFamily: {
         averiaSerifLibre: ['"Averia Serif Libre"', "serif"],
         inter: ['"Inter"', "sans-serif"],
-      }
+      },
     },
   },
   plugins: [],
