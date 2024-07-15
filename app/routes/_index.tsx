db7446fa79e6f0469b9b3ad25183299d3a9b534c
Michelle Brown <m.andreabr@gmail.com>
2024-07-15 13:53:35 +1000

    update meta

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -4,8 +4,8 @@ import Hero from "~/components/Hero";
 
 export const meta: MetaFunction = () => {
   return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
+    { title: "PrismLens" },
+    { name: "description", content: "AI Tarot Card Reader" },
   ];
 };
 
