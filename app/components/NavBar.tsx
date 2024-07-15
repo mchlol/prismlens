ec0d0e0f3113f7f798d7a6dc558fcae5be336092
Michelle Brown <m.andreabr@gmail.com>
2024-07-15 13:31:58 +1000

    import logo

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -1,8 +1,17 @@
+import Logo from "../assets/logo.svg";
+import { Link } from "@remix-run/react";
+
 export default function NavBar() {
     return (
-        <nav>
-            <p>PrismLens</p>
-            <ul>
+        <nav className="flex justify-between p-4 border-b-2">
+            <div>
+                <Link to="/" className="flex gap-1">
+                    <h1 className="font-averiaSerifLibre">PrismLens</h1>
+                    <img src={Logo} alt="logo" className="max-w-4" />
+                </Link>
+                
+            </div>
+            <ul className="flex gap-4">
                 <li>About</li>
                 <li>Resources</li>
             </ul>
