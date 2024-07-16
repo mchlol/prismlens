57a10dceb2aa6e63a196eb2d2927d7cd72e4a2e2
Michelle Brown <m.andreabr@gmail.com>
2024-07-16 18:22:19 +1000

    add hover state and transition

--- app/components/ReadingCard.tsx
+++ app/components/ReadingCard.tsx
@@ -5,7 +5,7 @@ type Props = {
 
 export default function ReadingCard(props: Props) {
     return (
-        <div className="w-64 border-2 p-8 rounded-3xl text-center">
+        <div className="w-64 border-2 p-8 rounded-3xl text-center transition hover:text-ridergreen">
             <h3 className="font-averiaSerifLibre text-lg mb-2">{props.title}</h3>
             <p>{props.content}</p>
         </div>
