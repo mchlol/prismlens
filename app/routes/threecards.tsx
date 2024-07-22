77beb5223816ec1944c7327353a033f0342b3c66
Michelle Brown <m.andreabr@gmail.com>
2024-07-22 17:44:11 +1000

    include AI report

--- app/routes/threecards.tsx
+++ app/routes/threecards.tsx
@@ -1,6 +1,8 @@
 import TarotCard from "~/components/TarotCard";
 import { LoaderFunction } from "@remix-run/node";
 import { useLoaderData, json } from "@remix-run/react";
+import { useEffect, useState } from "react";
+import { fetchReport } from "~/functions/functions";
 
 type CardObject = {
     desc: string
@@ -28,8 +30,28 @@ export let loader: LoaderFunction = async () => {
 export default function ThreeCards() {
 
     const { cards } = useLoaderData<LoaderData>();
+    const [report, setReport] = useState<string | null>('');
+    const [error, setError] = useState<string | null>('');
+    const [revealed, setRevealed] = useState<boolean>(false);
 
-    console.log(cards[0].name, cards[1].name, cards[2].name);
+    const getReport = async () => {
+        const cardNames = `For a 'Past, Present, Future' reading: ${cards[0].name}, ${cards[1].name}, ${cards[2].name}`;
+
+        try {
+            const result = await fetchReport(cardNames);
+            setReport(result);
+            setError(null);
+        } catch (err: any) {
+            setError(err.message);
+            setReport('');
+        }
+    }
+
+    useEffect( () => {
+        getReport();
+        console.log(report);
+    },[cards]);
+
     
     return (
         <section className="text-center">
@@ -38,10 +60,29 @@ export default function ThreeCards() {
             {
                 cards 
                 ? 
-                <div className="flex flex-row flex-wrap place-content-center gap-4">
-                 {cards.map(card => <TarotCard key={card.name_short} card={card} />)}
+                <div className="cards-wrapper">
+                    <h3 className="font-averiaSerifLibre text-lg">Your Cards</h3>
+
+                    <div className="flex flex-row flex-wrap place-content-center gap-4">
+                        {cards.map(card => <TarotCard key={card.name_short} card={card} />)}
+                    </div>
+
+                    <button 
+                    className="border-2 p-2 rounded-lg" 
+                    onClick={() => setRevealed(prevRevealed => !prevRevealed)}>
+                        {`${revealed ? 'Hide ' : 'Reveal '}`} Report
+                     </button>
+
                 </div>
                 : <p>Unable to retrieve card</p>
+            }
+
+            {
+                revealed
+                ?
+                report &&
+                <p className="max-w-[800px] mx-auto fade-in-text p-4">{report}</p>
+                : null
             }
             
 
