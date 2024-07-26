01aa21f09861af04d372e5ba727e12d39b8c3ee3
Michelle Brown <m.andreabr@gmail.com>
2024-07-26 15:54:51 +1000

    remove top border

--- app/components/Readings.tsx
+++ app/components/Readings.tsx
@@ -3,7 +3,7 @@ import { Link } from "@remix-run/react"
 
 export default function Readings() {
     return (
-        <section className="w-full m-4 p-8 mx-auto border-t-2 border-ridercream">
+        <section className="w-full m-4 p-8 mx-auto">
             <h2 className="text-3xl font-averiaSerifLibre mb-4 text-ridercream">Readings</h2>
 
             <div className="flex flex-wrap gap-4 justify-start">
