f43d003e9817c37a2f92bdcf209adfa8817ad79d
Michelle Brown <m.andreabr@gmail.com>
2024-07-26 15:54:11 +1000

    heading font and round corner

--- app/components/ReadingCard.tsx
+++ app/components/ReadingCard.tsx
@@ -15,12 +15,12 @@ export default function ReadingCard({children, imgPosition, rotate}: Props) {
         position = 'top-[-150px] -right-[150px]'
     } 
     return (
-        <div className="w-64 p-8 h-80 rounded-3xl text-ridercream bg-[url('/meshgradbg-duo.png')] bg-cover hover:scale-[1.03] transition ease-in-out relative overflow-hidden shadow-sm">
+        <div className="w-64 p-8 h-80 rounded-tr-[125px] text-ridercream bg-[url('/meshgradbg-duo.png')] bg-cover hover:scale-[1.03] transition ease-in-out relative overflow-hidden shadow-sm">
             <div className={`reading-card__img absolute ${position} rotate-[${rotate}] blur-[2px]`}>
                 <img src={StarCircleOutline} alt="" />
             </div>
             
-            <p className="absolute bottom-0 left-0 p-8 text-2xl">{children}</p>
+            <p className="absolute bottom-0 left-0 p-8 text-2xl font-averiaSerifLibre">{children}</p>
         </div>
     )
 }
