9eb57f4fb0f0682e205de85cb1f131021c3d165c
Michelle Brown <m.andreabr@gmail.com>
2024-07-29 13:00:01 +1000

    redesign hero

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -3,17 +3,20 @@ import StarCircleOutline from "../assets/starcircle-outline.svg"
 
 export default function Hero() {
     return (
-        <div className="hero p-8 flex justify-center items-center flex-wrap gap-8 relative mt-8">
+        <div className="hero flex justify-center items-center flex-wrap gap-8 h-[calc(100vh-10rem)] relative text-purplegrey overflow-hidden">
 
-            <div>
-                <img src={LogoFull} alt="logo" className="max-w-[500px] mb-8" />
+            <div className="z-20 p-4">
+               
+                <img src={LogoFull} alt="logo" className=" mb-8 svg" />
 
-                <div className="hero--text text-ridercream font-averiaSerifLibre text-xl w-96 text-center mx-auto">
+                <div className="hero--text font-averiaSerifLibre text-xl max-w-[500px] text-center mx-auto">
                     <p>Explore intuitive readings that help you navigate life's challenges and uncover new perspectives.</p>
                 </div>
             </div>
 
-            <img src={StarCircleOutline} alt="" className="-z-10 absolute -top-50 h-[220%] mx-auto rotate-45 svg" />
+            <div className="bg-[url('./assets/hero-bg.jpg')] bg-cover h-[500px] max-w-[500px] absolute left-0 right-0 mx-auto rounded-tr-full rounded-tl-full opacity-80 bordered border-2 border-ridercream"></div>
+
+            <img src={StarCircleOutline} alt="" className="-z-10 absolute -top-50 h-[180%] mx-auto rotate-45 svg-cream" />
 
         </div>
     )
