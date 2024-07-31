c4ca922db72343ae49cb5c40815419f78aa8631d
Michelle Brown <m.andreabr@gmail.com>
2024-07-31 17:04:27 +1000

    add scale on hover effect

--- app/components/CardSmall.tsx
+++ app/components/CardSmall.tsx
@@ -20,7 +20,7 @@ const CardSmall = ({card}: TarotCardProps) => {
     const imagePath = `/cards/${getImgCode(card.name_short)}.jpg`;
 
     return (
-        <Link to={`./${card.name}`} className="flex flex-col justify-center items-center gap-2 mb-2 max-w-[120px]">
+        <Link to={`./${card.name}`} className="flex flex-col justify-center items-center gap-2 mb-2 max-w-[120px] hover:scale-[1.03] transition ease-in-out">
             <img src={imagePath} className="w-full" />
             <p>{card.name === 'Fortitude' ? 'Strength' : card.name === 'The Last Judgment' ? 'Judgement' : card.name}</p>
         </Link>
