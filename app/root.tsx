7544c83ebed2cb99f5fa71402e111627222d3252
Michelle Brown <m.andreabr@gmail.com>
2024-08-15 12:00:35 +1000

    fix favicon 500 error on routes by starting with fwd slash

--- app/root.tsx
+++ app/root.tsx
@@ -16,7 +16,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <title>PrismLens</title>
-        <link rel="icon" type="image/png" href="favicon.ico" />
+        <link rel="icon" type="image/png" href="/favicon.ico" />
         <Meta />
         <Links />
       </head>
