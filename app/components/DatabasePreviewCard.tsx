2cd450195c810229eb2808339a7f75b9a86d279a
Michelle Brown <m.andreabr@gmail.com>
2024-08-16 16:17:34 +1000

    add database to home page

---
+++ app/components/DatabasePreviewCard.tsx
@@ -0,0 +1,18 @@
+import { Link } from "@remix-run/react";
+import { getImgCode } from "~/functions/functions";
+
+type Props = {
+    code: string
+    rotate: string
+}
+
+export default function DatabasePreviewCard(props: Props) {
+
+
+
+    return (
+        <Link to={`/tarotdatabase/${props.code}`}>
+            <img src={`./cards/${getImgCode(props.code)}.jpg`} alt='tarot card' className={`db-preview-card rotate-[${props.rotate}] transition-all duration-[400ms] hover:transform-none max-w-[200px] min-w-[150px] rounded-lg`}/>
+        </Link>
+    )
+}
