ac0e57215d8639dbf8f12f7b0e07929da0be8911
Michelle Brown <m.andreabr@gmail.com>
2024-08-16 16:23:16 +1000

    add img to background

--- app/components/Database.tsx
+++ app/components/Database.tsx
@@ -1,6 +1,7 @@
 import CardSmall from "~/components/CardSmall";
 import DatabaseSection from "~/components/DatabaseSection";
 import { Link } from "@remix-run/react";
+import StarCircleOutline from "../assets/starcircle-outline.svg"
 
 type CardObject = {
     desc: string
@@ -25,44 +26,49 @@ export default function Database(props: CardsArr) {
     return (
 
         <div className="database-wrapper mb-4">
-            <div className="section-heading m-8 mx-auto flex flex-col gap-4">
-                <h2 className="uppercase text-5xl font-averiaSerifLibre text-blueblack">Tarot Database</h2>
+
+            <div className="img-wrapper relative">
+                <img src={StarCircleOutline} className="-z-10 absolute -top-32 right-2 opacity-50 svg-cream max-w-[80%]" alt="" />
+            </div>
+
+            <div className="section-heading m-8 mx-auto flex flex-col gap-4 p-4">
+                <h2 className="uppercase text-3xl md:text-5xl font-averiaSerifLibre text-purplegrey">Tarot Database</h2>
                 <p className="max-w-[50ch] mx-auto">The Tarot deck is made up of 78 cards; 22 Major Arcana, and 56 Minor Arcana split into four suits of 14 cards each - Ace to Ten, and the court cards: Page, Knight, Queen and King.</p>
             </div>
 
             <div className="database-container flex relative bg-[url('/meshgradbg-duo.png')] bg-cover text-ridercream">
-                <div className="sticky top-4 p-4 h-[calc(100vh-2rem)] overflow-auto w-24 mt-4 ml-4 mr-4">
+                <div className="sticky top-4 p-4 h-[calc(100vh-2rem)] overflow-auto w-48 mt-4 ml-4 mr-4">
                     <h2>Jump to:</h2>
                     <ul className="space-y-2">
                         <li>
                             <Link to="#major"
-                            className="text-pink hover:text-ridercream">
+                            className="text-pink hover:text-ridercream transition-all duration-200 ease-in-out">
                                 Major Arcana
                             </Link>
                         </li>
                         <li>
                             <Link to="#minor"
-                            className="text-pink hover:text-ridercream">Minor Arcana
+                            className="text-pink hover:text-ridercream transition-all duration-200 ease-in-out">Minor Arcana
                             </Link>
                         </li>
                         <li>
                             <Link to="#swords"
-                            className="text-pink hover:text-ridercream">Swords
+                            className="text-pink hover:text-ridercream transition-all duration-200 ease-in-out">Swords
                             </Link>
                         </li>
                         <li>
                             <Link to="#wands"
-                            className="text-pink hover:text-ridercream">Wands
+                            className="text-pink hover:text-ridercream transition-all duration-200 ease-in-out">Wands
                             </Link>
                         </li>
                         <li>
                             <Link to="#pentacles"
-                            className="text-pink hover:text-ridercream">Pentacles
+                            className="text-pink hover:text-ridercream transition-all duration-200 ease-in-out">Pentacles
                             </Link>
                         </li>
                         <li>
                             <Link to="#cups"
-                            className="text-pink hover:text-ridercream">Cups
+                            className="text-pink hover:text-ridercream transition-all duration-200 ease-in-out">Cups
                             </Link>
                         </li>
                     </ul>
