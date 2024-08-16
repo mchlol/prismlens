17a2c8ebd8a9cbb1e046b6498b3692e09c9a9d7a
Michelle Brown <m.andreabr@gmail.com>
2024-08-16 17:08:09 +1000

    set hover styles using state

--- app/components/DatabasePreviewCard.tsx
+++ app/components/DatabasePreviewCard.tsx
@@ -1,4 +1,5 @@
 import { Link } from "@remix-run/react";
+import { useState } from "react";
 import { getImgCode } from "~/functions/functions";
 
 type Props = {
@@ -8,12 +9,21 @@ type Props = {
 
 export default function DatabasePreviewCard(props: Props) {
 
-
+    const [isHovered, setIsHovered] = useState(false);
 
     return (
         <Link to={`/tarotdatabase/${props.code}`}>
-            <img src={`./cards/${getImgCode(props.code)}.jpg`} alt='tarot card' className={`db-preview-card transition-all duration-[400ms] hover:transform-none max-w-[200px] min-w-[150px] rounded-lg`}
-            style={{ transform: `rotate(${props.rotate})` }}/>
+            <img 
+            src={`./cards/${getImgCode(props.code)}.jpg`} 
+            alt='tarot card' 
+            className={`db-preview-card transition-all duration-[400ms] max-w-[200px] min-w-[150px] rounded-lg`}
+            onMouseEnter={() => setIsHovered(true)}
+            onMouseLeave={() => setIsHovered(false)}
+            style={{ 
+                transform: isHovered ? 'none' : `rotate(${props.rotate})`, 
+                transition: 'transform 400ms ease-in-out'
+                }}
+            />
         </Link>
     )
 }
