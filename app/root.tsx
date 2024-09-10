16f4e398150aeca354e8f2c6d5a79d4b1b76fe9f
Michelle Brown <m.andreabr@gmail.com>
2024-09-10 11:05:47 +1000

    condense layout into default export

--- app/root.tsx
+++ app/root.tsx
@@ -9,32 +9,28 @@ import "./tailwind.css";
 import NavBar from "./components/NavBar";
 import Footer from "./components/Footer";
 
-export function Layout({ children }: { children: React.ReactNode }) {
+export default function App() {
   return (
     <html lang="en">
-      <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <title>PrismLens</title>
-        <link rel="icon" type="image/png" href="/favicon.ico" />
-        <Meta />
-        <Links />
-      </head>
-      <body className="mt-[6rem] font-inter flex flex-col justify-between min-h-[100vh] max-w-[1240px] mx-auto leading-relaxed text-blueblack grad-serenity">
-        <div className="w-full bg-purplegrey fixed top-0 left-0 z-30 blurnav">
-          <NavBar />
-        </div>
-        <main className="min-h-[calc(100vh-8rem)]">
-          {children}
-        </main>
-        <Footer />
-        <ScrollRestoration />
-        <Scripts />
-      </body>
-    </html>
-  );
-}
-
-export default function App() {
-  return <Outlet />;
+    <head>
+      <meta charSet="utf-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <title>PrismLens</title>
+      <link rel="icon" type="image/png" href="/favicon.ico" />
+      <Meta />
+      <Links />
+    </head>
+    <body className="mt-[6rem] font-inter flex flex-col justify-between min-h-[100vh] max-w-[1240px] mx-auto leading-relaxed text-blueblack grad-serenity">
+      <div className="w-full bg-purplegrey fixed top-0 left-0 z-30 blurnav">
+        <NavBar />
+      </div>
+      <main className="min-h-[calc(100vh-8rem)]">
+        <Outlet />
+      </main>
+      <Footer />
+      <ScrollRestoration />
+      <Scripts />
+    </body>
+  </html>
+  )
 }
