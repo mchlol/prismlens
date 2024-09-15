6dd98ff3b5dbecafa7515f3a1e8c31880854f381
Michelle Brown <m.andreabr@gmail.com>
2024-09-15 11:40:05 +1000

    display the names and images of cards

--- app/routes/readings.report.tsx
+++ app/routes/readings.report.tsx
@@ -1,13 +1,14 @@
 import { LoaderFunction } from "@remix-run/node";
 import { useLoaderData, json } from "@remix-run/react";
 import { useState, useEffect } from "react";
-import { fetchReport } from "~/functions/functions";
+import { fetchReport, getShortName } from "~/functions/functions";
 import HomeButton from "~/components/HomeButton";
 
 type LoaderData = {
     report: string
     type: string
     cardsForReading: string
+    cardsArr: Array<String>
 }
 
 export let loader: LoaderFunction = async ({request}) => {
@@ -19,19 +20,23 @@ export let loader: LoaderFunction = async ({request}) => {
     // construct the message to be sent to the API
     const message = `For a ${type} reading: ${cardsForReading}`;
     const report = await fetchReport(message);
+
+    const cardsArr = cardsForReading.split(', ');
     
-    return json<LoaderData>( {report, type, cardsForReading} );
+    return json<LoaderData>( {report, type, cardsForReading, cardsArr } );
 }
 
 
 export default function ReadingReport() {
 
-    const { report, type, cardsForReading } = useLoaderData<LoaderData>();
+    const { report, type, cardsForReading, cardsArr } = useLoaderData<LoaderData>();
+    console.log(cardsArr);
 
     const [reading, setReading] = useState('');
 
     useEffect( () => {
         setReading(report);
+        console.log(getShortName(cardsArr[0]))
     },[]);
 
     return (
@@ -39,11 +44,18 @@ export default function ReadingReport() {
             <h1 className="uppercase text-2xl md:text-5xl font-averiaSerifLibre mb-2 text-purplegrey">Your Reading</h1>
             <h2 className="text-xl text-purplegrey font-averiaSerifLibre">{type}</h2>
 
-            <div className="reading-cards fade-in-text p-4">
+            <div className="reading-cards text-purplegrey fade-in-text p-4">
                 <p>{cardsForReading}</p>
+
+                <div className="flex gap-4 justify-center p-8 border-pink">
+                    {
+                        cardsArr.map(card => <img key={card}className="max-w-32" src={`/cards/${getShortName(card)}.jpg`} alt="" />)
+                    }
+                </div>
             </div>
 
-            <div className="flex flex-col justify-center items-center gap-4 p-8 max-w-[75ch] mx-auto">
+            <div className="flex flex-row justify-center items-center gap-4 p-8 max-w-[75ch] mx-auto w-fit border-4 mb-4">
+                <img src="/dalle-portrait.webp" className="max-w-40 rounded-lg -hue-rotate-60" />
                 <p>{report}</p>
             </div>
 
